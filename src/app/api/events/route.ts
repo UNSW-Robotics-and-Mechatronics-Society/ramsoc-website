5544d86018107bbb92c98974965b9b5cbafcdfb9
Weichen Tie <weichentie@gmail.com>
2025-01-07 03:18:24 +1100

    added basic serverside error handing

--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -1,17 +1,31 @@
+import { MetaGraphAPIEventResponse } from "@/types/events";
+import axios, { AxiosError } from "axios";
 import { type NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
 
   const cursor = searchParams.get("cursor");
-  const res = await fetch(
-    `https://graph.facebook.com/v21.0/me/events?access_token=${process.env.META_API_PAGE_ACCESS_TOKEN}&fields=cover%2Cdescription%2Cend_time%2Cstart_time%2Cplace%2Cname%2Cid&limit=25${cursor ? `&after=${cursor}` : ""}`,
-  );
-  const data = await res.json();
-  return Response.json({
-    data: data.data,
-    paging: { cursors: data.paging?.cursors },
-  });
+  try {
+    const res = await axios.get<MetaGraphAPIEventResponse>(
+      `https://graph.facebook.com/v21.0/me/events?access_token=${process.env.META_API_PAGE_ACCESS_TOKEN}&fields=cover%2Cdescription%2Cend_time%2Cstart_time%2Cplace%2Cname%2Cid&limit=25${cursor ? `&after=${cursor}` : ""}`,
+    );
+    const data = res.data;
+    return Response.json({
+      data: data.data,
+      paging: { cursors: data.paging?.cursors },
+    });
+  } catch (err: any) {
+    if (err.response.headers.get("www-authenticate").includes(`cursor`)) {
+      return new Response("Invalid Cursor", { status: 400 });
+    } else if (
+      err.response.headers.get("www-authenticate").includes("invalid_token")
+    ) {
+      return new Response("Internal Server Error", { status: 503 });
+    }
+
+    return new Response("Internal Server Error", { status: 500 });
+  }
 }
 
 export const runtime = "edge";
