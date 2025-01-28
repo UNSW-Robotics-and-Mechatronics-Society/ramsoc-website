3f4d97121caf6fd7effb43f603e33e0dac3c06ee
alanchoi00 <alanchoi.uni@gmail.com>
2025-01-28 17:26:13 +0800

    feat: Add FlagshipEventsSection (skeleton) to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import EventsSection from "@/app/_components/EventsSection";
 import HeroSection from "@/app/_components/HeroSection";
 import MeetTheTeamSection from "@/app/_components/MeetTheTeamSection";
 import SponsoredSection from "@/app/_components/SponsoredSection";
+import FlagshipEventsSection from "./_components/FlagshipEventsSection";
 
 export default function HomePage() {
   return (
     <main>
       <HeroSection></HeroSection>
       <AboutUsSection></AboutUsSection>
+      <FlagshipEventsSection></FlagshipEventsSection>
       <EventsSection></EventsSection>
       <MeetTheTeamSection></MeetTheTeamSection>
       <SponsoredSection></SponsoredSection>
