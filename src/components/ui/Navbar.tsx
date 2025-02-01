dd8865e61211a4ada92ef21dee5396356283cc6e
Weichen Tie <weichentie@gmail.com>
2025-02-01 21:36:33 +1100

    made navbar mobile responsive and minor responsiveness changes on hero

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,10 +1,31 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { LuMenu } from "react-icons/lu";
+
+import { cn } from "@/lib/utils"
+
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "./Sheet";
+
 
 export default function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(window.scrollY > 10);
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 10);
+    };
+
+    window.addEventListener('scroll', handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
   return (
-    <nav className="absolute z-50 h-24 w-full">
-      <div className="mx-auto flex size-full max-w-[1200px] items-center text-primary-50">
+    <nav className={cn("fixed top-0 left-0 z-50 h-24 w-full px-8 transition-colors ease-out", isScrolled ? "bg-primary-950":"bg-primary-transparent")}>
+      <div className="mx-auto hidden size-full max-w-[1200px] items-center text-primary-50 sm:flex">
         <Image
           src="/logo.svg"
           alt="logo for ramsoc"
@@ -42,6 +63,47 @@ export default function Navbar() {
           </Link>
         </div>
       </div>
+      <Sheet>
+        <SheetTrigger className="ml-auto flex h-full items-center gap-2 text-xl text-white sm:hidden">
+          <LuMenu size={32} />
+        </SheetTrigger>
+          <SheetContent className="bg-primary-950 ">
+            <SheetHeader>
+                <SheetTitle hidden>Navbar for RAMSOC</SheetTitle>
+                <SheetDescription hidden>Navbar for RAMSOC</SheetDescription>
+                <div className="ml-auto flex w-full flex-col text-2xl text-white">
+                  <Link
+                    href="/"
+                    className="flex h-full items-center p-4 hover:bg-black/50"
+                    aria-label="Go to home page"
+                  >
+                    Home
+                  </Link>
+                  <Link
+                    href="/events"
+                    className="flex h-full items-center p-4 hover:bg-black/50"
+                    aria-label="Go to events page"
+                  >
+                    Events
+                  </Link>
+                  <Link
+                    href="/teams"
+                    className="flex h-full items-center p-4 hover:bg-black/50"
+                    aria-label="Go to team page"
+                  >
+                    Team
+                  </Link>
+                  <Link
+                    href="/#contact"
+                    className="flex h-full items-center p-4 hover:bg-black/50"
+                    aria-label="Go to contact us page"
+                  >
+                    Contact Us
+                  </Link>
+              </div>
+            </SheetHeader>
+          </SheetContent>
+      </Sheet>
     </nav>
   );
 }
