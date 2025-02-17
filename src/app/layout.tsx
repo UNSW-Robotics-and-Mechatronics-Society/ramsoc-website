556e8b84ab6545b4cb3fc9becb8583c5a9f968ab
Weichen Tie <weichentie@gmail.com>
2025-02-17 13:03:19 +1100

    fixed seo tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,11 +6,31 @@ import Navbar from "@/components/ui/Navbar";
 
 import Providers from "./providers";
 
+const title = "RAMSoc UNSW";
+const description =
+  "UNSW Robotics and Mechatronics Society (RAMSoc) is a student-run engineering society that aims to provide Mechatronic Engineering opportunities and pathways between mechatronic students and the professional community.";
+const url = "https://ramsocunsw.org";
+const image = "https://ramsocunsw.org/og.svg";
+
 export const metadata: Metadata = {
   title: "RAMSoc UNSW",
   description:
     "UNSW Robotics and Mechatronics Society (RAMSoc) is a student-run engineering society that aims to provide Mechatronic Engineering opportunities and pathways between mechatronic students and the professional community.",
   icons: { icon: "/favicon.svg", apple: "/favicon.svg" },
+  openGraph: {
+    url,
+    type: "website",
+    title,
+    description,
+    images: image,
+  },
+  twitter: {
+    card: "summary_large_image",
+    images: image,
+    description,
+    site: url,
+    title,
+  },
 };
 
 export default function RootLayout({
