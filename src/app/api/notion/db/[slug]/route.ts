b477e0f55e1a37450bfc4893032dfb1918ec6e0c
alanchoi00 <alanchoi.uni@gmail.com>
2025-09-21 14:48:33 +1000

    refactor(api): update GET handler to use dynamic database ID and environment token

--- src/app/api/notion/db/[slug]/route.ts
+++ src/app/api/notion/db/[slug]/route.ts
@@ -1,9 +1,25 @@
 import { Client } from "@notionhq/client";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: Request) {
-  const dbKey = "redacted";
+export async function GET(
+  request: NextRequest,
+  { params }: { params: Promise<{ slug: string }> },
+) {
+  const { slug: databaseId } = await params;
+  if (!databaseId) {
+    return new NextResponse("Missing Notion database ID", { status: 400 });
+  }
+  const notionToken = process.env.NOTION_TOKEN;
+  if (!notionToken) {
+    return new NextResponse("Missing Notion token", { status: 500 });
+  }
+
   const notion = new Client({
-    auth: "redacted",
+    auth: notionToken,
   });
-  return Response.json(await notion.databases.query({ database_id: dbKey }));
+
+  const db_result = await notion.databases.query({
+    database_id: databaseId,
+  });
+  return NextResponse.json(db_result);
 }
