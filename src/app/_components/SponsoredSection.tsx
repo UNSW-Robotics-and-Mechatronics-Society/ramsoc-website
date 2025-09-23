cffc6a6d021561efdf8e747d5d73aeaa7d9ca73b
Alan Choi <115921805+alanchoi00@users.noreply.github.com>
2025-09-23 18:46:30 +1000

    feat(SponsoredSection): add Pure Matcha logo to sponsored section (#35)
    * feat(SponsoredSection): add Pure Matcha logo to sponsored section
    
    * fix(SponsoredSection): add missing key prop to Pure Matcha logo div

--- src/app/_components/SponsoredSection.tsx
+++ src/app/_components/SponsoredSection.tsx
@@ -17,30 +17,41 @@ export default function SponsoredSection() {
             alt="unsw arc logo"
             width={100}
             height={100}
-          ></Image>,
+          />,
           <Image
             key={"unsw engineering logo"}
             src="/home/unsw-engineering-logo.png"
             alt="unsw engineering logo"
             width={100}
             height={100}
-          ></Image>,
+          />,
           <Image
             key={"unsw founders logo"}
             src="/home/unsw-founders-logo.png"
             alt="unsw founders logo"
             width={100}
             height={100}
-          ></Image>,
+          />,
           <Image
             key={"engineers australia logo"}
             src="/home/engineers-australia-logo.png"
             alt="engineers australia logo"
             width={100}
             height={100}
-          ></Image>,
+          />,
+          <div
+            key={"pure matcha logo"}
+            className="flex size-full items-center justify-center"
+          >
+            <Image
+              src="/home/purematcha-logo.avif"
+              alt="pure matcha logo"
+              width={100}
+              height={100}
+            />
+          </div>,
         ]}
-      ></InfiniteMovingCards>
+      />
     </Container>
   );
 }
