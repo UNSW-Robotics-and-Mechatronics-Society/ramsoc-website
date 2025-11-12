c1bb8ae507a4a6eb12d79b8465af6804b713117e
alanchoi00 <alanchoi.uni@gmail.com>
2025-11-12 12:42:11 +1100

    feat: enable mocking in Providers component

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,15 @@
 "use client";
 import { env } from "@/env";
+import { initMocks } from "@/mocks/init";
 import { TRPCReactProvider } from "@/trpc/react";
 import { GoogleTagManager } from "@next/third-parties/google";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Analytics } from "@vercel/analytics/next";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
+  if (env.NEXT_PUBLIC_ENABLE_MOCKING === "true") {
+    initMocks();
+  }
   return (
     <>
       <GoogleTagManager gtmId={env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID} />
