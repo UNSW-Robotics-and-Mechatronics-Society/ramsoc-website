36302fff7b57b998308aa13700aad16d35ed5335
laracquet <168734270+laracquet@users.noreply.github.com>
2026-04-08 20:52:09 +1000

    feat: merging sponsors lists into one array (#99)
    Merged sponsor lists from main and sponsors page into one array.

---
+++ src/features/sponsors/data.ts
@@ -0,0 +1,58 @@
+export interface Sponsor {
+  name: string;
+  logo: string;
+  url: string;
+}
+
+export const CURRENT_SPONSORS: Sponsor[] = [
+  {
+    name: "UNSW Arc",
+    logo: "/home/unsw-arc-logo.png",
+    url: "https://www.arc.unsw.edu.au/",
+  },
+  {
+    name: "UNSW Engineering",
+    logo: "/home/unsw-engineering-logo.png",
+    url: "https://www.engineering.unsw.edu.au/",
+  },
+  {
+    name: "UNSW School of Mechanical & Manufacturing Engineering",
+    logo: "/home/UNSW_MME_logo.jpeg",
+    url: "https://www.unsw.edu.au/engineering/our-schools/mechanical-and-manufacturing-engineering",
+  },
+  {
+    name: "UNSW School of Electrical Engineering and Telecommunications",
+    logo: "/home/UNSW_EET_logo.png",
+    url: "https://www.unsw.edu.au/engineering/our-schools/electrical-engineering-telecommunications",
+  },
+  {
+    name: "UNSW Founders",
+    logo: "/home/unsw-founders-logo.png",
+    url: "https://www.founders.unsw.edu.au/",
+  },
+  {
+    name: "Engineers Australia",
+    logo: "/home/engineers-australia-logo.png",
+    url: "https://www.engineersaustralia.org.au/",
+  },
+  {
+    name: "Jinro",
+    logo: "/home/Jinro_logo_-_no_background.png",
+    url: "https://jinro.com.au/",
+  },
+  {
+    name: "Pure Matcha",
+    logo: "/home/purematcha-logo.avif",
+    url: "https://purematcha.com.au/",
+  },
+  {
+    name: "Yokogawa",
+    logo: "/home/yokogawa_logo.png",
+    url: "https://www.yokogawa.com/au/",
+  },
+  {
+    name: "KOKO amuesment",
+    logo: "/home/koko.jpg",
+    url: "https://kokoamusement.com.au/",
+  },
+];
